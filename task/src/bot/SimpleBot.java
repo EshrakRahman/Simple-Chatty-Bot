048b6c9aa720941f1e2b2e1c9a75c24591a46cb8
Eshrak Rahman <eshrakurrahman@gmail.com>
2020-11-26 14:44:42 +0600

    Update SimpleBot.java

--- task/src/bot/SimpleBot.java
+++ task/src/bot/SimpleBot.java
@@ -3,7 +3,7 @@ package bot;
 import java.util.Scanner;
 
 public class SimpleBot {
-    final static Scanner scanner = new Scanner(System.in); 
+    final static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         greet("Aid", "2020");
@@ -11,7 +11,7 @@ public class SimpleBot {
         guessAge();
         count();
         test();
-        // ...
+        
         end();
     }
 
